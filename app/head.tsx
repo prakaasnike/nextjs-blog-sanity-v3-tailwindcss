e22208cd0940c45562a67b49296e02350d105dd5
Prakash Nayak <erprakashnayak@gmail.com>
2023-03-19 11:35:47 +0545

    style folder

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@
 export default function Head() {
     return (
         <>
-            <title>The Blog</title>
+            <title>Medium Nextjs Clone</title>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
             <link rel="icon" href="/favicon.ico" />
         </>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function HomePage() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        welcome to the blog
       </h1>
     </div>
   )

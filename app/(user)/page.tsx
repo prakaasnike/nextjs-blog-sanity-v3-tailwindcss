015853689a383ace333d61d380b18f56595d638b
Prakash Nayak <erprakashnayak@gmail.com>
2023-03-20 21:47:51 +0545

    preview mode checked sanity

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,12 @@
-
+import { previewData } from "next/headers"
 export default function HomePage() {
+  if (previewData()) {
+    return <div>previewData</div>
+  }
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-        welcome to the blog
+        Not in preview
       </h1>
     </div>
   )
